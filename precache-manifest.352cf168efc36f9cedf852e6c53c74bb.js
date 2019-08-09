fea6683eb3fef302dd2727c4f94f41ebde4ceb19
Nicholas Loperena <nicholasloperena@gmail.com>
2019-08-09 14:00:55 -0400

    Updates

---
+++ precache-manifest.352cf168efc36f9cedf852e6c53c74bb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcc82c5c1cd0daaf708e3b28225299d",
+    "url": "/Portfolio2/index.html"
+  },
+  {
+    "revision": "52274825f02dd2d2eee2",
+    "url": "/Portfolio2/static/css/main.6f5e4b58.chunk.css"
+  },
+  {
+    "revision": "5a093a0e0f419fbb34a5",
+    "url": "/Portfolio2/static/js/2.ca4ee930.chunk.js"
+  },
+  {
+    "revision": "52274825f02dd2d2eee2",
+    "url": "/Portfolio2/static/js/main.90805bf6.chunk.js"
+  },
+  {
+    "revision": "9d869cc5c6a35476c8a0",
+    "url": "/Portfolio2/static/js/runtime~main.32625e0a.js"
+  },
+  {
+    "revision": "4d13370f2bdfd8e6682599498ceefe54",
+    "url": "/Portfolio2/static/media/Nico.4d13370f.png"
+  },
+  {
+    "revision": "8288f4804ee24c2f0085cc5c3c8d1114",
+    "url": "/Portfolio2/static/media/Shiba.8288f480.gif"
+  },
+  {
+    "revision": "666c4c7f8b81da66baeff400810067d3",
+    "url": "/Portfolio2/static/media/Stars.666c4c7f.mp4"
+  },
+  {
+    "revision": "ec2d28bc29309ea8b0d44c25568e77cd",
+    "url": "/Portfolio2/static/media/aron-visuals-LSFuPFE9vKE-unsplash.ec2d28bc.jpg"
+  },
+  {
+    "revision": "525e03e5a9de4ca6873adfe5c9f22c5f",
+    "url": "/Portfolio2/static/media/cap.525e03e5.png"
+  },
+  {
+    "revision": "6ee408217e8b42f7e8ef9bd07eb12d7d",
+    "url": "/Portfolio2/static/media/woodpanels.6ee40821.jpg"
+  }
+]);
